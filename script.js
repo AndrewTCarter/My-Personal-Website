43169524d3b45546909c49dd12c893c4cc8705d2
AndrewTCarter <Andrewkarter@gmail.com>
2022-03-27 17:25:49 -0400

    Adding some more HTML and CSS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Glad you chose to stop on by!";
 
 document.querySelector("body").appendChild(h2);
